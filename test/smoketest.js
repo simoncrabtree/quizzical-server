743a360da7c8204a4fddb20f4c32e81b1d41969f
Simon Crabtree <simon@crabsticks.net>
2016-09-04 11:28:58 +0100

    Added immutable library and test

--- test/smoketest.js
+++ test/smoketest.js
@@ -1,7 +1,18 @@
 import {expect} from 'chai'
+import {List} from 'immutable'
 
 describe('smoke test', () => {
 	it('passes', () => {
 		expect(true).to.equal(true)
 	})
+})
+
+describe('Immutable List', () => {
+  it('is immutable', () => {
+    let state = List.of('A','B')
+    let nextState = state.push('C')
+
+    expect(state).to.equal(List.of('A','B'))
+    expect(nextState).to.equal(List.of('A','B','C'))
+  })
 })
