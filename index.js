00c2095fb4e88d332269df14b329bc618079506a
Simon Crabtree <simon@crabsticks.net>
2016-09-12 22:51:34 +0100

    Got socket.io sending messages to client

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import makeStore from './src/store'
 import startServer from './src/server'
+import { SET_QUIZ } from './src/actions'
 
 const store = makeStore()
 startServer(store)
+
+store.dispatch({type: SET_QUIZ, quiz: {
+  rounds: [
+    {
+      name: 'Round 1',
+      questions: []
+    }
+  ]
+}})
--- src/server.js
+++ src/server.js
@@ -4,6 +4,11 @@ export default function startServer(store) {
   const io = new Server().attach(8090);
 
   store.subscribe(
-    () => io.emit('state', store.getState().toJS())
+    () => io.emit('state', store.getState())
   )
+
+  io.on('connection', (socket) => {
+    console.log("Client Connected", socket.id)
+    socket.emit('state', JSON.stringify(store.getState()))
+  })
 }
